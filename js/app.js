5c9a010ec6204acfc0578fbdd69ad950080fa193
fullyslick <default@mail.com>
2018-04-16 17:25:43 +0300

    Add enemy class properties

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(posX, posY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = posX;
+    this.y = posY;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
