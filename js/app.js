d2c3fd637cebfcdbc93489c18639347a75ee28d3
fullyslick <default@mail.com>
2018-04-17 12:55:10 +0300

    Add dots to the comments

--- js/app.js
+++ js/app.js
@@ -10,58 +10,58 @@ var Enemy = function( posY) {
     // Holds the starting y postion of enemies.
     this.y = posY;
 
-    // These are the 3 options of the y postion where the enemy can reapear when it leaves the canvas
+    // These are the 3 options of the y postion where the enemy can reapear when it leaves the canvas.
     this.yOptions = [225, 142, 59];
 
-    // Returns a random value from the yOptions
+    // Returns a random value from the yOptions.
     this.changeY = function(){
       return this.yOptions[Math.floor(Math.random() * this.yOptions.length)];
     };
 
-    // There are 3 different values for speed: slow, medium, fast
+    // There are 3 different values for speed: slow, medium, fast.
     this.speedOptions = [200, 300, 400];
 
-    // Returns a random value from the speedOptions
+    // Returns a random value from the speedOptions.
     this.chooseSpeed = function(){
       return this.speedOptions[Math.floor(Math.random() * this.speedOptions.length)];
     };
 
     // Assign the return value of chooseSpeed() method to the speed property of the Enemy objects,
-    // so when it first appear it will get 1 of the 3 posibble speeds
+    // so when it first appear it will get 1 of the 3 posibble speeds.
     this.speed = this.chooseSpeed();
 
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a helper we've provided to easily load images.
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Parameter: dt, a time delta between ticks
+// Parameter: dt, a time delta between ticks.
 Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
   this.x += this.speed * dt;
 
-  // When enemy goes out the canvas it should reappear again on left
+  // When enemy goes out the canvas it should reappear again on left.
   if (this.x > canvas.width) {
 
   // Set the enemy to the left most position ( defaultX ).
   this.x = this.defaultX;
 
-  // Assign random Y postion of enemy from the 3 posibble options
+  // Assign random Y postion of enemy from the 3 posibble options.
   this.y = this.changeY();
 
-  // And change the speed of the enemy when it reapers
+  // And change the speed of the enemy when it reapers.
   this.speed = this.chooseSpeed();
   }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen, required method for game.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Now write your own player class.
 var Player = function(){
   this.x = 202;
   this.y = 380;
@@ -76,7 +76,7 @@ Player.prototype.render = function() {
 };
 
 // Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Parameter: dt, a time delta between ticks.
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -84,7 +84,7 @@ Player.prototype.update = function(dt) {
 };
 
 // Now instantiate your objects.
-// Creating enemy objects
+// Creating enemy objects.
 const bugOne = new Enemy(59);
 
 // Place all enemy objects in an array called allEnemies
